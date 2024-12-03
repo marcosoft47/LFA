ca8a0807c455758428fb588e64528807f7cf48b8
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2024-12-02 23:39:36 -0300

    implementado todos os automatos

--- TF.py
+++ TF.py
@@ -1,64 +1,20 @@
+from copy import deepcopy
 import random
+from automatos import *
 
-quintupla ={
-    'alfabeto': ["c","b", "e", "d", "p"],
-    'estados': ["A", "B", "C", "D", "c1c", "c1b", "c1e", "c2e", "c2b", "c2c", "c3c", "c3b", "c3d", "c4c", "c4b", "c4d", "r2", "r3", "p1", "p2", "p3", "p4", "p5"],
-    'programa': [
-        [None, "c1c", None, None, "p1"],
-        [None, "c2c", None, None, "p2"],
-        [None, None, None, "c1e", None],
-        [None, None,"c4d", None, None],
-        [None, "c3c", None, "c2e", None],
-        ["p1", None, None, "c2e", None],
-        ["p1", "c3c", None, "c2e", None],
-        ["p2", None, "c4c", "p3", None],
-        ["p2", None, None, "p3", None],
-        [None, 'c4c', "p3", None, None],
-        [None, "r2", "p5", None, None],
-        ["c1b", None, "p5", None, None],
-        ["c1b", "r2", "p5", None, None],
-        [None, "r3", "c3d", None, "p4"],
-        ["c2b", None, "c3d", None, "p4"],
-        ["c2b", "r3", "c3d", None, "p4"],
-        ["c3b", None, None, None, None],
-        ["c4b", None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None],
-        [None, None, None, None, None]],
-    'inicial': "A",
-    'final': ["p1", "p2", "p3", "p4", "p5"]
-}
-
+escolhasAutomatos = [caso1A, caso1B, caso1C, caso1D, caso2A,caso2B,caso2C,caso2D, completo]
 semaforo = ["c2e", "c2b", "c2c", "c3c", "c3b", "c3d"]
 semaforoEstado = False
 pilha = 0
 MAXIMOPILHA = 5
 
-def fitaValida(fita:str, alfabeto:list) -> tuple[bool,str]:
-    '''
-        Verifica se a fita possui elementos pertencentes no alfabeto
-        Retorna:
-            Bool: Se a fita é válida
-            Str: Caractere inválido na fita
-    '''
-    for i in fita:
-        if not i in alfabeto:
-            return (False,i)
-    return (True,'')
+carros: list[Automato]
+carros = []
 
-def caminhar(estadoAtual:str, caminho:str, quintupla:dict) -> str:
-    '''
-        Percorre no autômato o caminho a partir do estado atual
-        Retorna:
-            Str: Novo estado atual após percorrer o caminho    
-    '''
+nFim = 0
+count = 0
+fita = ''
 
-    alfabetoIndice = quintupla['alfabeto'].index(caminho)
-    estadoIndice = quintupla['estados'].index(estadoAtual)
-    return quintupla['programa'][estadoIndice][alfabetoIndice]
-    
 def verificaSemaforo(estadoAtual:str, semaforo:list):
     '''
         Verifica se foi passado em um semáforo, se sim, empilha
@@ -69,28 +25,124 @@ def verificaSemaforo(estadoAtual:str, semaforo:list):
         pilha+=1
         print(f"empilhou: {pilha}")
         if pilha >= MAXIMOPILHA:
+            print('Pilha cheia! Mudando o sinal do semáforo')
             pilha = 0
             semaforoEstado = not semaforoEstado
 
+def informarCarros(auto:bool):
+    count = 0
+    print(f'Informe qual caso você quer usar no carro {count+1}:')
+    print('1 - Caso 1A')
+    print('2 - Caso 1B')
+    print('3 - Caso 1C')
+    print('4 - Caso 1D')
+    print('5 - Caso 2A')
+    print('6 - Caso 2B')
+    print('7 - Caso 2C')
+    print('8 - Caso 2C')
+    print('9 - Caso Completo')
+    print('0 - Fim escolhas')
 
-estadoAtual = random.choice('ABCD')
-
-while not estadoAtual in quintupla["final"]:
-    estadoAnterior = estadoAtual
+    carros: list[Automato]
+    carros = []
     while True:
-        estadoAtual=random.choice(quintupla["programa"][quintupla["estados"].index(estadoAnterior)])
-        if estadoAtual != None:
+        escolha = int(input(f'Escolha o caso para o carro {count+1}: '))
+        if escolha == 0:
             break
-    # estadoAtual = caminhar(estadoAtual, caminho,quintupla)
-    verificaSemaforo(estadoAtual, semaforo)
-    print(estadoAtual)
+        count += 1
+        carros.append(escolhasAutomatos[escolha-1])
+        if escolha == len(escolhasAutomatos):
+            escolha = input('Escolha qual será o estado inicial (A, B, C ou D): ')
+            carros[-1].inicial = escolha.capitalize()
+        if not auto:
+            carros[-1].fita=input('Informe a fita do carro: ')
+        carros[-1].nome = f'Carro {count}'
+        print()
+    return carros
 
-print()
-print()
+print('Simulador automato com pilhas')
+print('Deseja fazer um teste automático (e aleatório), ou manual?')
+print('(1) Teste Manual\n(2) Teste automático')
+escolha = int(input())
 
-if estadoAtual in quintupla['final']:
-    print(f'Estado {estadoAtual} é um estado final')
-    print('Fita válida!')
-else:
-    print(f'Estado {estadoAtual} não é um estado final')
-    print('Fita inválida!')
+if escolha == 1:
+    print('Teste Manual')
+    
+    carros = informarCarros(auto=False)
+    print('Começando teste:')
+
+
+    count = 0
+    while True:
+        print(f"Loop {count+1}")
+        for i in carros:
+            if i.finalizou:
+                continue
+
+            print(f'Carro atual: {i.nome} -> ', end='')
+
+            if not i.fita[count] in i.alfabeto:
+                print(f'Elemento {i.fita[count]} não pertence no alfabeto de {i.nome}')
+                i.finalizou = True
+                nFim += 1
+                continue
+
+            i.percorrer(i.fita[count])
+            print(i.estadoAtual)
+
+            if i.estadoAtual == None:
+                print(f'Caminho inválido para o {i.nome}!')
+                i.finalizou = True
+
+            verificaSemaforo(i.estadoAtual, semaforo)
+            if len(i.fita) <= count+1:
+                print(f'Fim da fita do {i.nome}')
+                i.finalizou = True
+                nFim += 1
+                continue
+            if i.ehEstadoFinal():
+                i.finalizou = True
+                nFim += 1
+                print('FIM')
+        if nFim >= len(carros):
+            break
+
+        count += 1
+        
+
+
+elif escolha == 2:
+    print('Teste automático')
+    input('Definir automaticamente os casos?\n1 - Sim\n2 - Não')
+
+    if escolha == 1:
+        qnt = int(input('Informe quantos carros você quer simular: '))
+        for i in range(qnt):
+            carros.append(deepcopy(random.choice(escolhasAutomatos)))
+            print(f'Carro {i}: {carros[-1].nome}')
+            carros[-1].nome = f'Carro {i}'
+    elif escolha == 2:
+        carros = informarCarros(auto=True)
+
+    print()
+    print()
+    while True:
+        count += 1
+        print(f"Loop {count}")
+        for i in carros:
+            if i.ehEstadoFinal():
+                continue
+            print(f'Carro atual: {i.nome} -> ', end='')
+            i.sorteiaCaminho()
+            print(i.estadoAtual)
+            verificaSemaforo(i.estadoAtual, semaforo)
+            if i.ehEstadoFinal():
+                nFim += 1
+                print('FIM')
+                break
+        if nFim >= len(carros):
+            break
+
+    print('Fitas:')
+    for i in carros:
+        print(f'{i.nome}: {i.fita}')
---
+++ automatos.py
@@ -0,0 +1,242 @@
+from copy import deepcopy
+import random
+
+class Automato():
+    def __init__(self,nome: str, alfabeto: list[str], estados: list[str], programa: list[list], inicial: str, final:list[str]):
+
+        self.nome = nome
+        self.alfabeto = alfabeto
+        self.estados = estados
+        self.programa = programa
+        self.inicial = inicial
+        self.final = final
+        self.fita=''
+        self.finalizou = False
+        
+        self.estadoAtual = inicial
+
+    def __deepcopy__(self, memo): # memo is a dict of id's to copies
+        id_self = id(self)        # memoization avoids unnecesary recursion
+        _copy = memo.get(id_self)
+        if _copy is None:
+            _copy = type(self)(
+                nome=deepcopy(self.nome, memo), 
+                estados=deepcopy(self.estados, memo),
+                programa=deepcopy(self.programa, memo),
+                inicial=deepcopy(self.inicial, memo),
+                final=deepcopy(self.final, memo),
+                alfabeto=deepcopy(self.alfabeto, memo))
+            memo[id_self] = _copy 
+        return _copy
+    
+    def proximoEstado(self, caminho: str) -> str:
+        '''
+            Retorna o proximo estado a partir do estado atual, com base no caminho passado.
+            Argumentos:
+                caminho: Qual caminho (do alfabeto) será percorrido
+            Retorno:
+                Proximo estado depois de percorrer o caminho (Retorna None se for um caminho inválido)
+        '''
+        alfabetoIndice = self.alfabeto.index(caminho)
+        estadoIndice = self.estados.index(self.estadoAtual)
+        return self.programa[estadoIndice][alfabetoIndice]
+
+    def percorrer(self, caminho: str):
+        '''
+            Define o estado atual para o próximo estado, com base no caminho definido.
+            Argumentos:
+                caminho: Qual caminho (do alfabeto) será percorrido
+        '''
+        self.estadoAtual = self.proximoEstado(caminho)
+    
+    def caminhoValido(self, caminho: str) -> bool:
+        '''
+            Verifica se o caminho para o estado atual é válido
+            Argumentos:
+                caminho: Caminho que será verificado
+        '''
+        return self.programa[self.estados.index(self.estadoAtual)][self.alfabeto.index(caminho)] != None
+    
+    def fitaValida(self, fita:str) -> tuple[bool,str]:
+        '''
+            Verifica se a fita possui elementos pertencentes no alfabeto
+            Retorna:
+                Bool: Se a fita é válida
+                Str: Caractere inválido na fita
+        '''
+        for i in fita:
+            if not self.caminhoValido(i):
+                return (False,i)
+        return (True,'')
+    
+    def sorteiaCaminho(self):
+        '''
+            Seleciona um caminho válido aleatório para percorrer
+        '''
+        while True:
+            aux=random.choice(self.alfabeto)
+            if self.caminhoValido(aux):
+                self.percorrer(aux)
+                self.fita += aux
+                break
+    
+    def ehEstadoFinal(self) -> bool:
+        return self.estadoAtual in self.final
+
+# Caso 1
+
+caso1A = Automato(
+    nome='Caso 1A',
+    alfabeto=['c','b','p'],
+    estados=['A','c1c','c1b','c3c','c3b','r2','p1'],
+    programa=[[None, 'c1c','p1'],
+              [None, 'c3c', None],
+              ['p1',None, None],
+              [None, 'r2', None],
+              ['c1b', None, None],
+              ['c3b',None, None],
+              [None, None, None]],
+    inicial='A',
+    final=['p1']
+)
+
+caso1B = Automato(
+    nome='Caso 1B',
+    alfabeto=['c','b','p'],
+    estados=['B','c2c','c2b','c4c','c4b','r3','p2'],
+    programa=[[None,'c2c','p2'],
+              [None,'c4c',None],
+              ['p2',None,None],
+              [None, 'r3', None],
+              ['c2b',None, None],
+              ['c4b', None, None],
+              [None, None, None]],
+    inicial='B',
+    final=['p2']
+)
+
+caso1C = Automato(
+    nome='Caso 1C',
+    alfabeto=['d'],
+    estados=['C','c1e','c2e','p3'],
+    programa=[['c1e'],
+              ['c2e'],
+              ['p3'],
+              [None]],
+    inicial='C',
+    final=['p3']
+)
+
+caso1D = Automato(
+    nome='Caso 1D',
+    alfabeto=['e','p'],
+    estados=['D','c4d','c3d','p4','p5'],
+    programa=[['c4d',None],
+              ['c3d','p4'],
+              ['p5',None],
+              [None,None],
+              [None,None]],
+    inicial='D',
+    final=['p4','p5']
+)
+
+# Caso 2
+
+caso2A = Automato(
+    nome='Caso 2A',
+    alfabeto=['c','b','e','p'],
+    estados=['A','c1c','c1b','c3c','c3b','r2','p1','p5'],
+    programa=[[None, 'c1c',None, 'p1'],
+              [None,'c3c',None,None],
+              ['p1',None,None,None],
+              [None,'r2','p5',None],
+              [None,None,None,None],
+              [None,None,None,None],
+              [None,None,None,None]],
+    inicial='A',
+    final=['p1','p5']
+)
+
+caso2B = Automato(
+    nome='Caso 2B',
+    alfabeto=['c','b','d','p'],
+    estados=['A','c2c','c2b','c4c','c4b','r3','p2','p3'],
+    programa=[[None,'c2c',None,'p2'],
+              [None,'c4c','p3',None],
+              ['p2',None,'p3',None],
+              [None,'r3',None,None],
+              ['c2b',None,None,None],
+              ['c4b',None,None,None],
+              [None,None,None,None],
+              [None,None,None,None]],
+    inicial='B',
+    final=['p2','p3']
+)
+
+caso2C = Automato(
+    nome='Caso 2C',
+    alfabeto=['c','b','d'],
+    estados=['C','c1e','c2e','c2b','c4c','c4b','r3','p2','p3'],
+    programa=[
+        [None,None,'c1e'],
+        [None,None,'c2e'],
+        ['p2','c4c','p3'],
+        ['p2',None,'p3'],
+        [None,'r3',None],
+        ['c2b',None,None],
+        ['c4b',None,None],
+        [None,None,None],
+        [None,None,None]],
+    inicial='C',
+    final=['p2','p3']
+)
+
+caso2D = Automato(
+    nome='Caso 2D',
+    alfabeto=['c','b','e'],
+    estados=['D','c1b','c3b','c3d','c4d','r2','p5'],
+    programa=[
+        [None,None,'c4d'],
+        ['p1',None,None],
+        ['c1b',None,'p5'],
+        ['c1b','r2','p5'],
+        [None,None,'c3d'],
+        ['c3b',None,None],
+        [None,None,None]
+    ],
+    inicial='D',
+    final=['p5']
+)
+
+# Caso 3
+completo = Automato(
+    nome='Caso 3',
+    alfabeto= ["c","b", "e", "d", "p"],
+    estados= ["A", "B", "C", "D", "c1c", "c1b", "c1e", "c2e", "c2b", "c2c", "c3c", "c3b", "c3d", "c4c", "c4b", "c4d", "r2", "r3", "p1", "p2", "p3", "p4", "p5"],
+    programa= [
+        [None, "c1c", None, None, "p1"],
+        [None, "c2c", None, None, "p2"],
+        [None, None, None, "c1e", None],
+        [None, None,"c4d", None, None],
+        [None, "c3c", None, "c2e", None],
+        ["p1", None, None, "c2e", None],
+        ["p1", "c3c", None, "c2e", None],
+        ["p2", None, "c4c", "p3", None],
+        ["p2", None, None, "p3", None],
+        [None, 'c4c', "p3", None, None],
+        [None, "r2", "p5", None, None],
+        ["c1b", None, "p5", None, None],
+        ["c1b", "r2", "p5", None, None],
+        [None, "r3", "c3d", None, "p4"],
+        ["c2b", None, "c3d", None, "p4"],
+        ["c2b", "r3", "c3d", None, "p4"],
+        ["c3b", None, None, None, None],
+        ["c4b", None, None, None, None],
+        [None, None, None, None, None],
+        [None, None, None, None, None],
+        [None, None, None, None, None],
+        [None, None, None, None, None],
+        [None, None, None, None, None]],
+    inicial= 'A',
+    final= ["p1", "p2", "p3", "p4", "p5"]
+)
